8c4cba6010d93281cc0b617993028bc101f38752
timcarey <timcarey1989@gmail.com>
2023-11-11 22:59:23 -0500

    commit'

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_14_171647) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,8 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_171647) do
 #   Unknown type 'active_storage_attachments' for column 'image'
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "blog_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
