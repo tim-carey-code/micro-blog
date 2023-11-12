a85ad3dca1bb26c5550e6ccf78a74cb7b94aeef9
timcarey <timcarey1989@gmail.com>
2023-11-12 11:18:21 -0500

    change url prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_controller.default_url_options = { host: 'https://timcarey.biz' }
+  config.action_controller.default_url_options = { host: 'https://timcarey.biz', protocol: 'https' }
 end
